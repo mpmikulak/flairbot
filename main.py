1845fb1a0dd9435cd9010bb4de35f509f020e10b
mpmikulak <mpmikulak@google.com>
2018-09-25 19:02:19 -0500

    rough draft

--- main.py
+++ main.py
@@ -2,7 +2,6 @@
 
 import praw
 import dataset
-import datetime
 
 # OAuth credentials
 CLIENT_ID = 'u2s7QyS8qkpmAA'
@@ -12,13 +11,13 @@ USERNAME = '' # Required to be in edit mode
 PASSWORD = '' # Required to be in edit mode
 
 # SubReddit info
-SUBREDDIT_NAME = 'ClashRoyaleTrade'
+SUBREDDIT_NAME = 'funny'
 
 # Keywords that will increase reputation
 KEYWORDS = ('+1')
 
 # Filler text that will appear with the reputation number
-FILLER = 'Reputation'
+FILLER = 'Reputation:'
 
 # Authentication function
 def authenticate():
@@ -43,7 +42,7 @@ def obtain_database():
     try:
         db = dataset.connect('sqlite:///rep.db')
     except Exception:
-        print("There was a problem connecting to the database.")
+        print("There was a problem connecting to the database.", Exception)
     print("Connected to the database.")
     return db
 
@@ -57,11 +56,12 @@ def get_rep(table, poster):
 
 # Add reputation to the poster in the database
 def add_rep(submission_table, reputation_table, comment):
-    taken = submission_table.find(comment.submission.id)
+    submission_id = comment.submission.id
+    taken = submission_table.find(submission_id)
     awardee = comment.submission.author.name
     if taken is None:
-        reputation_table.insert(dict(name=awardee, rep = get_rep(reputation_table, awardee) + 1))
-        submission_table.insert(dict(name=comment.submission.id, used=True))
+        reputation_table.insert(dict(name=awardee, rep=get_rep(reputation_table, awardee) + 1))
+        submission_table.insert(dict(name=submission_id, used=True))
 
 def main():
     # Authenticate with Reddit and obtain a reddit instance
@@ -79,29 +79,13 @@ def main():
     # Manage the reddit instance   
     print("Beginning stream...")         
     for comment in reddit.subreddit(SUBREDDIT_NAME).stream.comments(skip_existing=True):
-        print("{}: {}".format(comment.author.name, comment.body))
+        
         if comment.body in KEYWORDS:
+            print("{}: {}: {}".format(comment.author.name, comment.body, comment.submission.id))
             poster = comment.submission.author.name
             add_rep(submission_table, reputation_table, comment)
             reddit.subreddit(SUBREDDIT_NAME).flair.set(poster, "{}{}".format(FILLER, get_rep(reputation_table, poster)))
             
-
-# def getRep(username):
-#   rep = table.find(name=username)
-#   if rep is None:
-#     return 0
-#   return int(rep['rep'])
-
-# def addRep(username, amount=1):
-#   table.insert(dict(name=username, rep = getRep() + 1))
-
-
-# for comment in reddit.subreddit('ClashRoyaleTrade').stream.comments():
-#     if comment.body in ("+rep", "+1"):
-#       op = comment.submission.author
-#       reddit.subreddit('ClashRoyaleTrade').flair.set(op, getRep(op))
-#       comment.reply("{}'s rep has been updated".format(op))
-
 if __name__ == "__main__":
     main()
 
